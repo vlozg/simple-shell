29a023024861e6f1311c4dcd65399c6cf944af0b
LongVu <55818681+vulong61@users.noreply.github.com>
2020-10-31 15:27:48 +0700

    initiate main file with sample code from teacher

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#define MAX LINE 80 /* The maximum length command */
+
+int main(void)
+{
+    char *args[MAX LINE/2 + 1]; /* command line arguments */
+    int should run = 1; /* flag to determine when to exit program */
+    while (should run) {
+        printf("osh>");
+        fflush(stdout);
+        /**
+        * After reading user input, the steps are:
+        * (1) fork a child process using fork()
+        * (2) the child process will invoke execvp()
+        * (3) parent will invoke wait() unless command included &
+        */
+    }
+    return 0;
+}
