73d4431940cd7c66eb2a9d15cbd0333cfc91bbd4
LongVu <55818681+vulong61@users.noreply.github.com>
2020-11-02 13:10:40 +0700

    Add descript, fix trailing end char & NULL end arr

--- parse_cmd.c
+++ parse_cmd.c
@@ -2,15 +2,35 @@
 #include <string.h>
 
 int parseCommand(char* line, char* cmd[])
+/*
+    Return number and array of arguments from user inputted command line.
+
+    Parameter:
+    -   line (char*): user inputted command line.
+    -   cmd (char* []): empty array used for returning arguments parsed from command line.
+
+    Return: (int) number of arguments parsed from command line.
+*/
 {
     char * pch;
-    int len = 0;
+    int len = strlen(line) - 1; // Use temporarily for length of line
 
+
+    //Remove trailing character at the end of line
+    while (line[len] == '\r' || line[len] == '\n' || line[len] == ' ')
+    {
+        line[len--] = '\0';
+        if (len < 0) break;
+    }
+
+
+    len = 0; // Change len back to the length of array of arguments
     pch = strtok(line," ");
     while (pch != NULL)
     {
         cmd[len++] = pch;
         pch = strtok (NULL, " ");
     }
+    cmd[len] = NULL; // Array of arguments must be NULL terminated ( exec() requirement )
     return len;
 }
