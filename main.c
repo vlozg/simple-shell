9e4579baea8d55c580478096219ff93ea61ae5e1
LongVu <55818681+vulong61@users.noreply.github.com>
2020-11-05 02:35:38 +0700

    Extract single command process to a function

--- main.c
+++ main.c
@@ -6,60 +6,79 @@
 #include <sys/wait.h>
 #define MAX_LINE 200 /* The maximum length command */
 
+int executeSingleCmd(char* line, int wait_flag);
+
 int main(void)
 {
-    char *args[MAX_LINE/2 + 1]; /* command line arguments */
     char line[MAX_LINE]; /* user command */
+    char tmp[MAX_LINE];
     int terminated = 0; /* flag to determine when to exit program */
-    int wait_flag = 1; /* flag to determine whether to wait for child process or not */
 
     while (!terminated) {
         wait_flag = 1;
         printf("osh>");
         fflush(stdout);
         
-        // Read user command line and parse
+        // Read user command line
         fgets(line, MAX_LINE, stdin);
-        int n_args = parseCommand(line, args);
-        
-        // Check for exit command
-        if (strcmp(args[0], "exit") == 0
-            || strcmp(args[0], "q") == 0
-            || strcmp(args[0], "quit") == 0)
-        {
-            terminated = 1;
-            continue;
-        }
 
-        // Check for & argument
-        
-        if (strcmp(args[n_args-1], "&") == 0)
-        {
-            wait_flag = 0;
-            args[--n_args] = NULL;
-        }
+        for (int i = 0, brk = 0; i < strlen(line); i++){
+            if (line[i] == '&')
+            {
+                
+            }
+            else if (line[j] == '|')
+            {
 
-        // Fork a child process
-        pid_t new_pid;
-        int status;
-        new_pid = fork();
-        switch (new_pid)
-        {
-            case -1: 
-                printf( "Cannot fork any new process." ); 
-                break;
-            case 0:
-                if (execvp(args[0], args) < 0) 
-                {
-                    printf("%s: command not found\n", args[0]);
-                    return 0;
-                }
-                break;
-            default:
-                if (wait_flag == 1)
-                    waitpid(new_pid ,&status, 0);
-                break;
+            }
         }
     }
     return 0;
+}
+
+int executeSingleCmd(char* line, int wait_flag)
+{
+    char *args[MAX_LINE/2 + 1]; /* command line arguments */
+    
+    // Redirect then parse arguments
+    parseRedirectCommand(line);
+    int n_args = parseCommand(line, args);
+    
+    // Check for exit command
+    if (strcmp(args[0], "exit") == 0
+        || strcmp(args[0], "q") == 0
+        || strcmp(args[0], "quit") == 0)
+    {
+        return 0;
+    }
+
+    // Check for history argument or !! argument
+    if (strcmp(args[0], "history") == 0
+        || strcmp(args[0], "!!") == 0)
+    {
+        return 0;
+    }
+
+    // Fork a child process
+    pid_t new_pid;
+    int status;
+    new_pid = fork();
+    switch (new_pid)
+    {
+        case -1: 
+            printf( "Cannot fork any new process." ); 
+            break;
+        case 0:
+            if (execvp(args[0], args) < 0) 
+            {
+                printf("%s: command not found\n", args[0]);
+                return 0;
+            }
+            break;
+        default:
+            if (wait_flag == 1)
+                waitpid(new_pid ,&status, 0);
+            break;
+    }
+    return 1;
 }
